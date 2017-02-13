79dd9f5a3cd59836dd4ff0e46b4a7122129e88d2
VoloBro <vova.prisyazhnuk@gmail.com>
2017-02-13 01:32:13 -0500

    bug fix for table completion

--- reportingsquad/runscompare/views.py
+++ reportingsquad/runscompare/views.py
@@ -17,6 +17,7 @@ def compareruns(request):
     # Retrieve the top 5 only - or all if less than 5.
     # Place the list in our context_dict dictionary which will be passed to the template engine.
     query_newrun = TestRun.objects.filter(run_id='2')
+    query_oldrun = TestRun.objects.filter(run_id='1')
 
     rows = {}
 
@@ -29,13 +30,17 @@ def compareruns(request):
     status_not_executed.name = 'n/a'
 
     for new_run in query_newrun:
-        old_run = TestRun.objects.filter(run_id='1', test_id=new_run.test_id)
-        if old_run: #if test_id found in old run
-            for old_run_item in old_run:
+        old_run_byid = query_oldrun.filter(test_id=new_run.test_id)
+        if old_run_byid: #if test_id found in old run
+            for old_run_item in old_run_byid:
                 rows[new_run.test_id.id] = [new_run.test_id.name, new_run.status, 0,  old_run_item.status, 0]
         else: #if test_id not found in old run - set status to not executed
             rows[new_run.test_id.id] = [new_run.test_id.name, new_run.status, 0, status_not_executed, 0]
-        pass
+
+    for old_run in query_oldrun:
+        if old_run.test_id.id not in rows.keys():
+            rows[old_run.test_id.id] = [old_run.test_id.name, status_not_executed, 0, old_run.status, 0]
+
 
     context_dict = {'newrun': query_newrun, 'rows': rows, 'dict': dict, 'list': list}
 
